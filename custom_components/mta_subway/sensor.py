ebdc8fe7cb8d1f713e2e4326c8a53bfa0492adab
Mickey Scherrer <scherrer@iicky.com>
2019-07-11 12:12:46 -0400

    Updated MTA sensor

--- custom_components/mta_subway/sensor.py
+++ custom_components/mta_subway/sensor.py
@@ -1,16 +1,15 @@
-'''
+"""
 Sensor for checking the status of NYC MTA Subway lines.
-'''
+"""
 
-from datetime import timedelta
 import logging
 import re
-
-from bs4 import BeautifulSoup as Soup
-import requests
-import voluptuous as vol
+from datetime import timedelta
 
 import homeassistant.helpers.config_validation as cv
+import requests
+import voluptuous as vol
+from bs4 import BeautifulSoup as Soup
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
@@ -48,10 +47,11 @@ SUBWAY_LINES = [
 ]
 
 STATE_PRIORITY = {
-    'Delays': 1,
-    'Service Change': 2,
-    'Planned Work': 3,
-    'Good Service': 4,
+    "Delays": 1,
+    "Service Change": 2,
+    "Local to Express": 3,
+    "Planned Work": 4,
+    "Good Service": 5,
 }
 
 URL = 'http://web.mta.info/status/ServiceStatusSubway.xml'
@@ -63,8 +63,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    ''' Sets up the MTA Subway sensors.
-    '''
+    """ Sets up the MTA Subway sensors.
+    """
     data = MTASubwayData()
     data.update()
     sensors = [
@@ -75,11 +75,11 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
 
 class MTASubwaySensor(Entity):
-    ''' Sensor that reads the status for an MTA Subway line.
-    '''
+    """ Sensor that reads the status for an MTA Subway line.
+    """
     def __init__(self, name, data):
-        ''' Initalize the sensor.
-        '''
+        """ Initalize the sensor.
+        """
         self._name = 'MTA Subway ' + str(name)
         self._line = name
         self._data = data
@@ -92,34 +92,34 @@ class MTASubwaySensor(Entity):
 
     @property
     def name(self):
-        ''' Returns the name of the sensor.
-        '''
+        """ Returns the name of the sensor.
+        """
         return self._name
 
     @property
     def state(self):
-        ''' Returns the state of the sensor.
-        '''
+        """ Returns the state of the sensor.
+        """
         return self._state
 
     @property
     def icon(self):
-        ''' Returns the icon used for the frontend.
-        '''
+        """ Returns the icon used for the frontend.
+        """
         return 'mdi:subway'
 
     @property
     def entity_picture(self):
-        ''' Returns the icon used for the frontend.
-        '''
+        """ Returns the icon used for the frontend.
+        """
         return '/local/mta_subway/{}.svg'.format(
             str(self._line).lower()
         )
 
     @property
     def device_state_attributes(self):
-        ''' Returns the attributes of the sensor.
-        '''
+        """ Returns the attributes of the sensor.
+        """
         attrs = {}
         attrs['direction_0_state'] = self._direction_0_state
         attrs['direction_1_state'] = self._direction_1_state
@@ -130,8 +130,8 @@ class MTASubwaySensor(Entity):
         return attrs
 
     def update(self):
-        ''' Updates the sensor.
-        '''
+        """ Updates the sensor.
+        """
         self._data.update()
         line_data = self._data.data[self._line]
         self._state = line_data['state']
@@ -145,8 +145,8 @@ class MTASubwaySensor(Entity):
 
 
 class MTASubwayData(object):
-    ''' Query MTA Subway data from the XML feed.
-    '''
+    """ Query MTA Subway data from the XML feed.
+    """
 
     def __init__(self):
 
@@ -154,20 +154,20 @@ class MTASubwayData(object):
 
     @Throttle(SCAN_INTERVAL)
     def update(self):
-        ''' Update data based on SCAN_INTERVAL.
-        '''
+        """ Update data based on SCAN_INTERVAL.
+        """
         response = requests.get(URL)
         if response.status_code != 200:
-            _LOGGER.warning("Invalid response from API")
+            _LOGGER.warning("Invalid response from MTA Subway XML feed.")
         else:
             self.data = parse_subway_status(response)
 
 
 def parse_subway_status(data):
-    ''' Returns a nested dictionary of MTA subway line statues
+    """ Returns a nested dictionary of MTA subway line statues
         given an XML response.
-    '''
-    # Set all line statuses to base status
+    """
+    # Set all line statuses to base status.
     line_status = {
         line: {
             'state': None,
@@ -183,83 +183,93 @@ def parse_subway_status(data):
     # Parse MTA lines from XML
     soup = Soup(data.text, 'xml')
 
-    # Iterate over line lookup and parse status
+    # Iterate over line lookup and parse status.
     for line in SUBWAY_LINES:
 
-        # Alias for Shuttle (S)
+        # Rename the MTA alias for Shuttle (S).
         line_alias = 'H' if line == 'S' else str(line)
 
-        # Search for line name in affected lines XML
+        # Search for line name in affected lines XML.
         line_re = re.compile('NYCT_' + line_alias + '$')
         hits = [
             _ for _ in soup.find_all('Affects')
             if _.findChildren('LineRef', text=line_re)
         ]
 
-        # Set line status to Good Service if no status
+        # Set line status to Good Service if no status.
         if not hits:
             line_status[line].update({
-                'state': 'Good Service',
-                'direction_0_state': 'Good Service',
-                'direction_1_state': 'Good Service',
+                'state': "Good Service",
+                'direction_0_state': "Good Service",
+                'direction_1_state': "Good Service",
             })
             continue
 
         # Parse all subway line situations that contain
-        # affected line
+        # affected line.
         situations = [
             _.find_parent('PtSituationElement')
             for _ in hits
         ]
 
-        # Parse subway line state
+        # Parse subway line state.
         statuses = [
             _.ReasonName.text
             for _ in situations
         ]
 
-        # Set the current state using the minimum of the
-        # ordinal STATE_PRIORITY dictionary
+        # Look for overlap of statuses with known states
+        # in STATE_PRIORITY dictionary
         matches = set(
             STATE_PRIORITY.keys()
         ).intersection(set(statuses))
 
-        line_status[line]['state'] = min(
-            {_: STATE_PRIORITY[_] for _ in matches},
-            key=STATE_PRIORITY.get
-        )
+        # Set the current state using the minimum of the
+        # ordinal STATE_PRIORITY dictionary, or unknown if
+        # state does not exist in dictionary.
+        if len(matches) > 0:
+            line_status[line]['state'] = min(
+                {_: STATE_PRIORITY[_] for _ in matches},
+                key=STATE_PRIORITY.get
+            )
+        else:
+            line_status[line]['state'] = 'Unknown'
 
-        # Determine state for each direction on the line
+        # Determine state for each direction on the line.
         dir_states = {
             '0': ['Good Service'],
             '1': ['Good Service'],
         }
         for sit in situations:
 
-            # Find affected line directions
+            # Find affected line directions.
             directions = [
                 _.DirectionRef.text
                 for _ in sit.find_all('AffectedVehicleJourney')
                 if _.findChildren('LineRef', text=line_re)
             ]
 
-            # Add states to line direction
+            # Add states to line direction.
             for dct in directions:
                 dir_states[dct].append(sit.ReasonName.text)
 
-        # Set the direction states using STATE_PRIORITY
+        # Set the direction states using STATE_PRIORITY.
         for dct in dir_states:
             matches = set(
                 STATE_PRIORITY.keys()
             ).intersection(set(dir_states[dct]))
 
             direction = 'direction_{}_state'.format(dct)
-            line_status[line][direction] = min(
-                {_: STATE_PRIORITY[_] for _ in matches},
-                key=STATE_PRIORITY.get
-            )
 
-        # Set line status descriptions
+            if len(matches) > 0:
+                line_status[line][direction] = min(
+                    {_: STATE_PRIORITY[_] for _ in matches},
+                    key=STATE_PRIORITY.get
+                )
+            else:
+                line_status[line][direction] = 'Unknown'
+
+        # Set line status descriptions.
         for status in STATE_PRIORITY:
             desc_key = (
                 status.lower().replace(' ', '_') +
@@ -278,3 +288,4 @@ def parse_subway_status(data):
                 )
 
     return line_status
+
